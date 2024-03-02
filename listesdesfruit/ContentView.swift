0743ec59a22e4940ba8d256338d14108ee4a360f
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-02 20:15:47 +0100

    en cours

--- listesdesfruit/ContentView.swift
+++ listesdesfruit/ContentView.swift
@@ -1,128 +1,102 @@
 //
-//  ContentView.swift
+//  FruitListView.swift
 //  listesdesfruit
 //
-//  Created by  Ixart on 04/12/2023.
+//  Created by  Ixart on 05/12/2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+        
     var body: some View {
-       
+        
+        
         NavigationStack{
            
 
             
-
+            
             
             ForEach(fruits) {fruit in
                         
                         
-                NavigationLink(destination: FruitDetailsView(unFruit: fruit)) {
+            NavigationLink {
                                 
                                 
                             
-                        
-                        
-                    HStack  {
-                        ZStack {
-                            Rectangle()
+                    FruitDetailsView(unFruit: fruit)
+                
+                } label: {
+                    
+                    
+                HStack  {
+                    ZStack {
+                        Rectangle()
 
-                                .frame(width: 90, height: 110)
-                                .foregroundColor(fruit.iconColor)
-                                .cornerRadius(20.0)
-                            Image(fruit.image)
-                            
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 90, height: 110)
-                            
-                        }
+                            .frame(width: 90, height: 110)
+                            .foregroundColor(fruit.iconColor)
+                            .cornerRadius(20.0)
+                        Image(fruit.image)
                         
-                        VStack(alignment: .leading){
-                            
-                            Text(fruit.nameDuFruit)
-                            
-                                .foregroundColor(.black)
-                                .bold()
-                                .font(.title2)
-                                .padding()
-                                .font(.system(size: 20))
-                            
-                            
-                           
-                            
-                            Text(fruit.texte)
-                                .multilineTextAlignment(.leading) // mettre  le texte a gauche
-
-                                .foregroundColor(.black)
-                                .font(.system(size: 12, weight: .light))
-                                
-                                .italic()
-                                .padding(.leading, 10)
-                                
-                                
-                        } // fin de VStack
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 90, height: 110)
                         
-                    } //fin de Hstack
+                    }
                     
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                } // finnavigationlink
-            
-            
-            
+                    VStack(alignment: .leading){
+                        
+                        Text(fruit.nameDuFruit)
+                        
+                            .foregroundColor(.black)
+                            .bold()
+                            .font(.title2)
+                            .padding()
+                            .font(.system(size: 20))
+                        
+                        
+                       
+                        
+                        Text(fruit.texte)
+                            .multilineTextAlignment(.leading) // mettre  le texte a gauche
 
+                            .foregroundColor(.black)
+                            .font(.system(size: 12, weight: .light))
+                            
+                            .italic()
+                            .padding(.leading, 10)
+                            
+                            
+                    } // fin de VStack
+                    
+                } //fin de Hstack
+                    
             }// fin du label
-            
+                
 
         }// fin du foreach
-
+            
     } // fin du navigationstack
-
-} // fin du body
-
-    
-    
-    
-    
-    
-    
-    
-            
-            
         
+    .padding()
+    .navigationTitle("fruit")
+
         
-       
-       
-       
+  } // fin du body
+    
+    
+    
+    
+    
+    
+    
+    
+} // fin de la struct
+
+        
 
 
-
-
-
-
-
-
-
-
-
-    #Preview {
-        ContentView()
+#Preview {
+    ContentView()
 }
-
-
-
-
-
